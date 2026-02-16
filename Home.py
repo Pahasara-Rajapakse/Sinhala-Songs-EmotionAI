42bc675e42bd1f0d0b4849d51bea8260dd7ef812
Pahsara Rajapakse <147439909+Pahasara-Rajapakse@users.noreply.github.com>
2026-02-16 14:39:18 +0530

    Update Home.py

--- Home.py
+++ Home.py
@@ -99,7 +99,7 @@ with st.sidebar:
     border=4, # Border එක අනිවාර්යයෙන් 4ක් තියෙන්න ඕනේ
     )
 
-    app_url = "https://sinhala-songs-emotion-ai.streamlit.app"
+    app_url = "https://sinhala-songs-emotion-ai.streamlit.app/Single_Song_Analyzer"
 
     qr.add_data(app_url)
     qr.make(fit=True)
